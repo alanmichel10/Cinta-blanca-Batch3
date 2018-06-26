ef74c38cb8a98d3229dcf757e371c354c5de3fec
alanmichel10 <alan_michel_10@hotmail.com>
2018-06-26 13:28:46 -0600

    nada

--- js/arrays.js
+++ js/arrays.js
@@ -23,5 +23,26 @@ if(palabra1.length==palabra2.length){
     }
 }
 
+function numeroMayor(arreglo){
+    arreglo=arreglo.sort(function(a,b){return a-b});
+    let mayor=arreglo[arreglo.length-1];
+    return mayor;
 
+}
 
+let numero=[
+    [1,2,3,4,5,3,32,54,65,43,76,12,76,98,35,333,12],
+    [1,2,3,4,5,3,32,54,65,43,76,12,76,98,35,333,12],
+    [1,2,3,4,5,3,32,54,65,43,76,12,76,98,35,333,12],
+]
+for(let i=0;i<numero.length;i++){
+    let mayor=numeroMayor(numero[i]);
+    console.log(mayor)
+}
+
+let numeros1=[];
+for(i=0;i<40;i++){
+    numeros1.push( Math.round(Math.random()*1000))
+}
+//console.log(numeros1);
+console.log(numeroMayor(numeros1));
