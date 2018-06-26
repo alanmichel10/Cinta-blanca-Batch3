de73b35396f490744fe6994036bae06bd9be401a
alanmichel10 <alan_michel_10@hotmail.com>
2018-06-26 13:27:23 -0600

    Se creo un codigo para generar la serie fibonacci con tantos numeros como uno quiera

---
+++ js/fibonacci.js
@@ -0,0 +1,45 @@
+function creaParrafoHTML(numero){
+    let parrafo=document.createElement('p');//<p></p>
+    parrafo.textContent=numero+',';
+    return parrafo
+}
+
+function fibonacci(numero){
+let fibo=[0,1];
+let contenedor=document.createElement('div');
+
+for(let i=1;i<numero-1;i++){
+    let numero=fibo[i]+fibo[i-1];
+    fibo.push(numero);
+
+    let parrafo=creaParrafoHTML(numero);
+    contenedor.appendChild(parrafo);
+    
+}
+console.log(fibo);
+
+document.getElementsByTagName('body')[0].appendChild(contenedor);
+
+return fibo;
+}
+function numerosPares(numeros){
+    let contador=0
+    for(i=0;i<=numeros.length;i++){
+    if(numeros[i]%2==0){
+        contador++;}
+    }
+ console.log(`tiene ${contador} numeros pares`)
+     
+}
+
+function calculaFibo(){
+     let cantidad=document.getElementById('cantidad').value;
+     if(cantidad>1){
+     fibonacci(cantidad);
+     }
+     else{
+         alert('Puro numero positivo alv compa')
+     }
+}
+
+
